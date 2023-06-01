a9f54d7e2d41b457bfd709e7e37112b30c1e9da0
João Pedro Rocha <jpcrocha8@gmail.com>
2023-06-01 16:40:13 -0300

    ajustes html e css footer e js agendamento

--- assets/js/formulario.script.js
+++ assets/js/formulario.script.js
@@ -1,5 +1,11 @@
 var postDiv;
 var postContainer;
+var armt = [];
+var armc = [];
+var armde = [];
+var arma = [];
+var armd = [];
+var i = 0;
 
 function adicionar() {
     let titulo = document.getElementById("titulo").value;
@@ -9,14 +15,15 @@ function adicionar() {
     let data = document.getElementById("data").value;
 
      postContainer = document.getElementById("posts");
-     postDiv = '<div class="post">' +
+     postDiv = '<div id="flex">' +
+                    '<div class="post">' +
                     '<h2 id="h3_post">' + titulo + '</h2>' +
                     '<p id="categoria_post">Categoria: ' + categoria + '</p>' +
                     '<p id="descricao_post">Assunto: ' + assunto + '</p>' +
                     '<p id="autor_post">Nome: ' + nome + '</p>' +
                     '<p id="data_post">Data agendada:' + data +'</p>' +
-                    '<button class="botao" onclick="removerPost()">remover</button>' +
-                    '</div>';
+                    '</div>' + '<div id="d_botao">' + '<span class="botao" onclick="removerPost()">remover</span>' +
+                    '</div>' + '</div>';
 
 postContainer.innerHTML += postDiv;
 
@@ -24,5 +31,11 @@ document.getElementById("titulo").value = "";
 document.getElementById("data").value = "";
 document.getElementById("assunto").value = "";
 document.getElementById("nome").value = "";
+}
+
+function removerPost(){
+    if(adicionar() == true){
+        i++
+    }
 }
 
